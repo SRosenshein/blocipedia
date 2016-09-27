daa57a6a8b4238f33529bfc08daf58d2ed2aabd8
Sam Rosenshein <srosenshein1@gmail.com>
2016-09-27 17:00:46 +0000

    user_policy setup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,15 @@ FactoryGirl.define do
   factory :user do
     email 'person@example.com'
     password 'helloworld'
+    password_confirmation 'helloworld'
     
+    trait :admin do
+      role 'admin'
+    end
+    
+    trait :premium do
+      role 'premium'
+    end
     
   end
 end
